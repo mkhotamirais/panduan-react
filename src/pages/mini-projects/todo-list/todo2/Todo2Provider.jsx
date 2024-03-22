8c646c8d8f36e264a882e46085062f001d1cf43d
Mkhotamirais <mkhotamirais@gmail.com>
2024-03-23 02:00:42 +0700

    coba lagi

--- src/pages/mini-projects/todo-list/todo2/Todo2Provider.jsx
+++ src/pages/mini-projects/todo-list/todo2/Todo2Provider.jsx
@@ -1,5 +1,28 @@
 import { createContext, useReducer } from "react";
-import { todoReducer } from "./Todo2Reducer";
+import { v4 as uuidv4 } from "uuid";
+
+const todoReducer = (todos, action) => {
+  switch (action.type) {
+    case "added": {
+      const result = [...todos, { id: uuidv4(), text: action.text, done: false }];
+      localStorage.setItem("todo2", JSON.stringify(result));
+      return result;
+    }
+    case "changed": {
+      const result = todos.map((t) => (t.id === action.todo.id ? action.todo : t));
+      localStorage.setItem("todo2", JSON.stringify(result));
+      return result;
+    }
+    case "deleted": {
+      const result = todos.filter((t) => t.id !== action.id);
+      localStorage.setItem("todo2", JSON.stringify(result));
+      return result;
+    }
+    default: {
+      throw Error(`Unknown action ${action.type}`);
+    }
+  }
+};
 
 // const initialTodo = [
 //   { id: "0", text: "hamad", done: true },
